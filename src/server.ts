67f868de3095b1021aff3be49adf714fbeef7c50
FranciscoVitorNunes <franciscovitornunes.programador@gmail.com>
2025-09-04 16:34:21 -0300

    Implementando dados

--- src/server.ts
+++ src/server.ts
@@ -8,13 +8,42 @@ app.register(cors,{
 })
 
 const drivers = [
-    {id:1, nome:"masr"},
+    { id: 1, nome: "Max Verstappen", teamId: 1 },
+    { id: 2, nome: "Sergio Pérez", teamId: 1 },
+    { id: 3, nome: "Charles Leclerc", teamId: 2 },
+    { id: 4, nome: "Carlos Sainz", teamId: 2 },
+    { id: 5, nome: "Lewis Hamilton", teamId: 3 },
+    { id: 6, nome: "George Russell", teamId: 3 },
+    { id: 7, nome: "Lando Norris", teamId: 4 },
+    { id: 8, nome: "Oscar Piastri", teamId: 4 },
+    { id: 9, nome: "Fernando Alonso", teamId: 5 },
+    { id: 10, nome: "Lance Stroll", teamId: 5 },
+    { id: 11, nome: "Esteban Ocon", teamId: 6 },
+    { id: 12, nome: "Pierre Gasly", teamId: 6 },
+    { id: 13, nome: "Alexander Albon", teamId: 7 },
+    { id: 14, nome: "Logan Sargeant", teamId: 7 },
+    { id: 15, nome: "Valtteri Bottas", teamId: 8 },
+    { id: 16, nome: "Zhou Guanyu", teamId: 8 },
+    { id: 17, nome: "Yuki Tsunoda", teamId: 9 },
+    { id: 18, nome: "Daniel Ricciardo", teamId: 9 },
+    { id: 19, nome: "Kevin Magnussen", teamId: 10 },
+    { id: 20, nome: "Nico Hülkenberg", teamId: 10 }
+];
 
-]
-const teams=[
-    {id:1, nome:"ferari"},
+const teams = [
+    { id: 1, nome: "Red Bull Racing" },
+    { id: 2, nome: "Ferrari" },
+    { id: 3, nome: "Mercedes" },
+    { id: 4, nome: "McLaren" },
+    { id: 5, nome: "Aston Martin" },
+    { id: 6, nome: "Alpine" },
+    { id: 7, nome: "Williams" },
+    { id: 8, nome: "Sauber (Stake)" },
+    { id: 9, nome: "RB (Visa Cash App)" },
+    { id: 10, nome: "Haas" }
+];
 
-]
+
 app.get('/teams',async(req,res)=>{
     res.type('application/json').code(200);
     return (teams)
