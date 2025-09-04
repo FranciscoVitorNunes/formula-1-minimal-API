5768886712cf31c7b1d0714925310f9bf59cddf6
FranciscoVitorNunes <franciscovitornunes.programador@gmail.com>
2025-09-04 16:12:32 -0300

    Criando endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,29 @@
 import fastify from 'fastify';
+import { Interface } from 'readline';
 
 const app = fastify({logger: true});
+const drivers = [
+    {id:1, nome:"masr"},
+    {id:2, nome:"masr"},
+    {id:3, nome:"masr"},
+    {id:4, nome:"masr"},
+]
+const teams=[
+    {id:1, nome:"ferari"},
+    {id:2, nome:"mercedes"},
+    {id:3, nome:"redull"},
+    {id:4, nome:"outra"},
+]
+app.get('/teams',async(req,res)=>{
+    res.type('application/json').code(200);
+    return (teams)
+})
+
+app.get('/drivers',async(req,res)=>{
+    res.type('application/json').code(200);
+    return (drivers)
+})
+
 
 
 app.listen({port: 3333}, ()=>{
